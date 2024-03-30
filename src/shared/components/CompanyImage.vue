4ba620d1b42150b7092d465e9465382ab66755d4
Gilmer J. Vizconde <gilmerjose.vizconde2@gmail.com>
2024-03-29 23:59:15 -0500

    fix image

--- src/shared/components/CompanyImage.vue
+++ src/shared/components/CompanyImage.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div class="w-full h-[162px] bg-red-600 overflow-hidden">
-    <img class="w-full h-auto -mt-20" src="../../../public/images/parking.jpeg" />
+    <img class="w-full h-auto -mt-20" src="/public/images/parking.jpg" />
   </div>
 </template>
 
